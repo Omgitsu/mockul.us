1307bb0b2730a4503819457e46dafbd72eaa5c1d
James Baker <omgitsu@gmail.com>
2015-07-10 17:37:53 -0700

    added download links, producthunt code/banner

---
+++ js/index.js
@@ -0,0 +1,16 @@
+(function() {
+  var $tryNowButton = $('.js-try-now-button');
+  var $getTheApp = $('.js-get-the-app');
+
+
+  $tryNowButton.on('click', {category:'download', action:'download-started', label:'hero-section' },downloadMockulus);
+  $getTheApp.on('click', {category:'download', action:'download-started', label:'header-link' },downloadMockulus);
+
+  function downloadMockulus(e) {
+    category = e.data.category
+    action = e.data.action
+    label = e.data.label
+    ga('send', 'event', category, action, label);
+    window.location.href = 'http://mockul.us/downloads/mockulus.app.zip';
+  };
+})();
