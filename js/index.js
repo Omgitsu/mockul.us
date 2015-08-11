e344e433797efe7460a1ae74a1c41d46173cb108
James Baker <omgitsu@gmail.com>
2015-08-10 22:29:14 -0700

    download link

--- js/index.js
+++ js/index.js
@@ -11,6 +11,6 @@
     action = e.data.action
     label = e.data.label
     ga('send', 'event', category, action, label);
-    window.location.href = 'http://mockul.us/downloads/mockulus.app.zip';
+    window.location.href = 'http://bit.ly/1UzHVnG';
   };
 })();
