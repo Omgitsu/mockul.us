f91295053212441e0f59621a79a55cc68df5f287
James Baker <omgitsu@gmail.com>
2016-07-31 12:42:12 -0700

    published Macaw file

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
 (function() {
   var $tryNowButton = $('.js-try-now-button');
+  var $downloadFromAppStore = $('.js-download-from-app-store');
   var $getTheApp = $('.js-get-the-app');
 
 
   $tryNowButton.on('click', {category:'download', action:'download-started', label:'hero-section' },downloadMockulus);
+
   $getTheApp.on('click', {category:'download', action:'download-started', label:'header-link' },downloadMockulus);
+
+  $downloadFromAppStore.on('click', {category:'download', action:'download-from-app-store-clicked', label:'hero-section' },downloadMockulusFromAppStore);
 
   function downloadMockulus(e) {
     category = e.data.category
@@ -12,5 +16,13 @@
     label = e.data.label
     ga('send', 'event', category, action, label);
     window.location.href = 'http://bit.ly/1iDJ8fn';
+  };
+
+  function downloadMockulusFromAppStore(e) {
+    category = e.data.category
+    action = e.data.action
+    label = e.data.label
+    ga('send', 'event', category, action, label);
+    window.location.href = 'http://apple.co/2aCDU4l';
   };
 })();
